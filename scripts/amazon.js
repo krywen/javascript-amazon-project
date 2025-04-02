80e1314dbe5108b380d82c14a15680cf988ee145
Lorenzo Belli <lorenzo.scientifico@gmail.com>
2025-04-02 22:26:13 +0200

    Make the cart qty interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -77,7 +77,13 @@ document.querySelectorAll(".js-add-to-cart-button").forEach((button) => {
         id: id,
         quantity: 1
       });
-    }
-    console.log(cart);
+    } 
+
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
   });
 });
